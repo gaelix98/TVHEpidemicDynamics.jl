6ad1a9ef521a2e98469a5285cf2798b0939c020c
aleant93 <aless.antelmi@gmail.com>
2020-02-03 14:03:35 +0100

    data distribution

--- src/TVHEpidemicDynamics.jl
+++ src/TVHEpidemicDynamics.jl
@@ -1,5 +1,13 @@
 module TVHEpidemicDynamics
 
-greet() = print("Hello World!")
+using DataFrames
+using Dates
+
+export evaluateintervals!, evaluatedensity!
+export evaluatedistance!, evaluate_direct_contacts_distribution!
+export evaluate_location_distribution!
+
+include("helper.jl")
+
 
 end # module
