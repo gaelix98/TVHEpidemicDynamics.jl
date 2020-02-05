b13c035fe5c1f1bdb344106bed94391214a404d7
aleant93 <aless.antelmi@gmail.com>
2020-02-05 09:53:01 +0100

    algorithm + direct vs indirect contagion experiment

--- src/TVHEpidemicDynamics.jl
+++ src/TVHEpidemicDynamics.jl
@@ -1,13 +1,19 @@
 module TVHEpidemicDynamics
 
+using CSV
 using DataFrames
 using Dates
+using SimpleHypergraphs
 
 export evaluateintervals!, evaluatedensity!
 export evaluatedistance!, evaluate_direct_contacts_distribution!
 export evaluate_location_distribution!
 
-include("helper.jl")
+export buildparamsdirect, buildhg, generatehg
+export TVHSIS
 
+include("helper.jl")
+include("epidemics/TVH.jl")
+include("epidemics/TVHSIS.jl")
 
 end # module
